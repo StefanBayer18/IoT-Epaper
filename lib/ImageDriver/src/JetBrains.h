b72a28894657d5db72db9523e3940d749e45271e
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2024-03-05 15:00:11 +0100

    Change font to bold

--- lib/ImageDriver/src/JetBrains.h
+++ lib/ImageDriver/src/JetBrains.h
@@ -8,9 +8,6 @@
 #include "./Font.h"
 
 struct JetBrains {
-    /// Font data for Jetbrains 16px
-    static const Font font16;
-
     /// Font data for Jetbrains 24px
     static const Font font24;
 };
